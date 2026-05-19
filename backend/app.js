41c4a995b74cb416f43f80c7339e650f4cada387
samybensalem7 <samybenss09@gmail.com>
2026-05-19 16:45:56 +0200

    optimisation du backend et preparation de la bd

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const Book = require('./models/Book');
+const bookRoutes = require('./routes/book');
 
 mongoose.connect('mongodb+srv://samybensalem_db_user:EJwo8lsKaKDgoRPx@cluster0.sssk3t2.mongodb.net/grimoire?appName=Cluster0')
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -18,42 +18,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/books', (req, res, next) => {
-   delete req.body._id;
-   const book = new Book({
-    ...req.body
-   });
-   book.save()
-   .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
-   .catch(error=> res.status(400).json({error}));
-});
-
-app.put('/api/books/:id', (req, res, next)=> {
-  Book.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id})
-  .then(()=> res.status(200).json({message: 'Livre modifié !'}))
-  .catch(error => res.status(400).json({error}));
-});
-
-app.delete('/api/books/:id', (req, res, next) => {
-  Book.deleteOne({_id: req.params.id})
-  .then(()=> res.status(200).json({ message: 'Livre supprimé !'}))
-  .catch(error => res.status(400).json ({error}));
-});
-
-app.get('/api/books/:id', (req, res, next) => { 
-  Book.findOne({_id: req.params.id })
-  .then(book => res.status(200).json(book))
-  .catch(error => res.status(404).json({error}));
-
-
-});
-
-app.get('/api/books', (req, res, next) => {
-  Book.find()
-    .then(books => res.status(200).json(books))
-    .catch(error => res.status(400).json({error}));
-});
-
+app.use('/api/books', bookRoutes);
 
 module.exports = app;
 
---
+++ backend/routes/book.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const bookCtrl = require('../controllers/book');
+
+router.post('/', bookCtrl.createBook );
+router.put('/:id', bookCtrl.modifyBook);
+router.delete('/:id', bookCtrl.deleteBook);
+router.get('/:id', bookCtrl.getOneBook );
+router.get('/', bookCtrl.getAllBooks);
+module.exports = router;
