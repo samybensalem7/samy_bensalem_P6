9f1d7feee7dd7556895e5a41c08a28be412abaaf
samybensalem7 <samybenss09@gmail.com>
2026-06-07 13:29:04 +0200

    modifications de la route put et de la route delete

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -8,7 +8,7 @@ const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBooks);
 router.post('/', auth, multer, bookCtrl.createBook );
-router.put('/:id', auth,  bookCtrl.modifyBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth,  bookCtrl.deleteBook);
 router.get('/:id', bookCtrl.getOneBook );
 
